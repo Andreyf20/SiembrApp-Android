b065bac32de2be4e91022f915c392224b974f066
Andrey Torres <atorresf20@gmail.com>
2020-11-20 17:48:36 -0600

    Added wildcards to filter

--- Scripts DB/Plantas Consultar Functions.sql
+++ Scripts DB/Plantas Consultar Functions.sql
@@ -21,6 +21,8 @@ RETURNS TABLE(
 )
 AS
 $$
+    DECLARE
+        filtro_wildcard varchar := (SELECT concat('%',filtro,'%'));
     BEGIN
 
         IF (filtro NOT LIKE '*') THEN
@@ -51,7 +53,7 @@ $$
                 LEFT JOIN fenologia FL ON P.idFenologia  = FL.idFenologia
                 LEFT JOIN agentePolinizador AP ON P.idAgentePolinizador = AP.idAgentePolinizador
                 LEFT JOIN metodoDispersion MD ON P.idMetodoDispersion = MD.idMetodoDispersion
-                WHERE LOWER(P.nombreComun) LIKE lower(filtro) or LOWER(P.nombreCientifico) LIKE lower(filtro) or LOWER(P.origen) LIKE lower(filtro) or LOWER(P.requerimientosDeLuz) LIKE lower(filtro) or LOWER(P.habito) LIKE lower(filtro) or LOWER(P.frutos) LIKE lower(filtro) or LOWER(P.texturaFruto) LIKE lower(filtro) or LOWER(P.flor) LIKE lower(filtro) or LOWER(P.usosConocidos) LIKE lower(filtro) or LOWER(P.paisajeRecomendado) LIKE lower(filtro) or LOWER(F.nombre) LIKE lower(filtro) or LOWER(FL.nombre) LIKE lower(filtro) or LOWER(AP.nombre) LIKE lower(filtro) or LOWER(MD.nombre) LIKE lower(filtro) or P.minRangoAltitudinal :: varchar = filtro or P.maxRangoAltitudinal :: varchar = filtro or P.metros :: varchar = filtro and P.borrado = False;
+                WHERE LOWER(P.nombreComun) LIKE lower(filtro_wildcard) or LOWER(P.nombreCientifico) LIKE lower(filtro_wildcard) or LOWER(P.origen) LIKE lower(filtro_wildcard) or LOWER(P.requerimientosDeLuz) LIKE lower(filtro_wildcard) or LOWER(P.habito) LIKE lower(filtro_wildcard) or LOWER(P.frutos) LIKE lower(filtro_wildcard) or LOWER(P.texturaFruto) LIKE lower(filtro_wildcard) or LOWER(P.flor) LIKE lower(filtro_wildcard) or LOWER(P.usosConocidos) LIKE lower(filtro_wildcard) or LOWER(P.paisajeRecomendado) LIKE lower(filtro_wildcard) or LOWER(F.nombre) LIKE lower(filtro_wildcard) or LOWER(FL.nombre) LIKE lower(filtro_wildcard) or LOWER(AP.nombre) LIKE lower(filtro_wildcard) or LOWER(MD.nombre) LIKE lower(filtro_wildcard) or P.minRangoAltitudinal :: varchar = filtro or P.maxRangoAltitudinal :: varchar = filtro or P.metros :: varchar = filtro and P.borrado = False;
         ELSE
             RETURN QUERY
                 SELECT
@@ -92,4 +94,5 @@ select * from spverplantas('*');
 select * from spverplantas('Saragundí');
 select * from spverplantas('senna reticulata');
 select * from spverplantas('500');
+select * from spverplantas('15m');
 */
