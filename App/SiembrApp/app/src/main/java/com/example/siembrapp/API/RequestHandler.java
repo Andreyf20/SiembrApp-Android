8ab2882d1bc020ce5d99061c9dcf3474d104fb59
Andrey Torres <atorresf20@gmail.com>
2021-01-20 19:16:27 -0600

    Changed url for testing

--- App/SiembrApp/app/src/main/java/com/example/siembrapp/API/RequestHandler.java
+++ App/SiembrApp/app/src/main/java/com/example/siembrapp/API/RequestHandler.java
@@ -66,7 +66,7 @@ public class RequestHandler {
 
     public static class APIRequester{
 
-        //private static final String APIURL = "http://192.168.0.3:5000/api/";
+        //private static final String APIURL = "http://192.168.0.2:5000/api/";
         private static final String APIURL = "https://siembrapptest.herokuapp.com/api/";
 
         public static void request(JSONObject params,Context ctx,int mode, VolleyCallBack callback){
@@ -240,8 +240,6 @@ public class RequestHandler {
         private static void login(JSONObject bodyParams, Context ctx, final VolleyCallBack callback){
             //Request url
             String url = APIURL + "login";
-
-            Log.d("mecagoendios", "login: "+url);
 
             //Instanciar Listener para el JsonObjectRequest
             Response.Listener<JSONObject> responseListener = new Response.Listener<JSONObject>() {
